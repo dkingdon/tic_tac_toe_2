832cfc4118475ccd5a07d0896a53c4f29f6d89b1
dkingdon <daniel.kingdon@gmail.com>
2017-11-21 23:00:54 -0800

    initial js set up. document ready function and started main on click function. My brain hurts.

---
+++ app.js
@@ -0,0 +1,11 @@
+// wait for the DOM to finish loading
+$(document).ready(function() {
+  // all code to manipulate the DOM
+  // goes inside this function
+
+  $('.col-md-4').click(function() {
+
+
+    
+  }); /* end of on click function */
+}); /* end of document ready function */
