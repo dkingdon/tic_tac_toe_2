2679d1e5aea462b018215b7157d9d5e0c529f11c
dkingdon <daniel.kingdon@gmail.com>
2017-11-21 23:12:40 -0800

    initial game logic set up, game piece logic

--- app.js
+++ app.js
@@ -2,10 +2,27 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  var targetID
+  var turnSymbol = "O"
 
   $('.col-md-4').click(function() {
+    /* grabs unique square id and updates the targetID variable */
+    targetID = (this.id);
 
+    /* checks to see if the square is clickable */
+    if (targetID !== "notAvail") {
 
-    
+      /* switches game piece */
+      if (turnSymbol === "O") {
+        turnSymbol = "X";
+      }
+      else {
+        turnSymbol = "O"
+      }
+
+      
+
+    };
+
   }); /* end of on click function */
 }); /* end of document ready function */
